735b00fe9852016316c9d55ecb748809c10b2d59
Jacob Lewallen <jlewallen@gmail.com>
2008-04-26 07:43:11 +0000

    Minor tweaking.

---
+++ DependencyStore/Services/IVersionProvider.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace DependencyStore.Services
+{
+  public interface IVersionProvider
+  {
+  }
+}
---
+++ DependencyStore/Services/Impl/VersionProvider.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace DependencyStore.Services.Impl
+{
+  public class VersionProvider : IVersionProvider
+  {
+  }
+}
