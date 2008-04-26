4b18b34901ea8c570e0b505a9b4c2a4ea0190cc8
Jacob Lewallen <jlewallen@gmail.com>
2008-04-26 07:54:08 +0000

    Going to bed...

--- DependencyStore/Services/Impl/VersionProvider.cs
+++ DependencyStore/Services/Impl/VersionProvider.cs
@@ -1,9 +1,16 @@
 using System;
 using System.Collections.Generic;
+using DependencyStore.Domain;
 
 namespace DependencyStore.Services.Impl
 {
   public class VersionProvider : IVersionProvider
   {
+    public void FindCommonVersion(IEnumerable<FileSystemFile> files)
+    {
+      foreach (FileSystemFile file in files)
+      {
+      }
+    }
   }
 }
