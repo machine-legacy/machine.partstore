f6b6318b2863656c3d04ae5324c957d28de8afb0
Jacob Lewallen <jlewallen@gmail.com>
2008-08-06 12:31:26 -0700

    Moving SynchronizationPlan to its own file.

--- DependencyStore/Domain/SynchronizationOperation.cs
+++ DependencyStore/Domain/SynchronizationOperation.cs
@@ -1,36 +1,7 @@
 using System;
-using System.Collections.Generic;
 
 namespace DependencyStore.Domain
 {
-  public class SynchronizationPlan : IEnumerable<SynchronizationOperation>
-  {
-    private readonly List<SynchronizationOperation> _operations = new List<SynchronizationOperation>();
-
-    public void AddOperation(SynchronizationOperation operation)
-    {
-      _operations.Add(operation);
-    }
-
-    public void Merge(SynchronizationPlan plan)
-    {
-      _operations.AddRange(plan);
-    }
-
-    #region IEnumerable<SynchronizationOperation> Members
-    public IEnumerator<SynchronizationOperation> GetEnumerator()
-    {
-      return _operations.GetEnumerator();
-    }
-    #endregion
-
-    #region IEnumerable Members
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-    {
-      return GetEnumerator();
-    }
-    #endregion
-  }
   public abstract class SynchronizationOperation
   {
   }
