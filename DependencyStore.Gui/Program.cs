4396f3f837f5d508bf25575a9242a29e4f9a6453
Jacob Lewallen <jlewallen@gmail.com>
2008-08-05 17:42:32 -0700

    Adding container and other application initialization code to GUI.

--- DependencyStore.Gui/Program.cs
+++ DependencyStore.Gui/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
+using DependencyStore.Domain.Configuration;
+
 namespace DependencyStore.Gui
 {
   public static class Program
@@ -9,9 +11,18 @@ namespace DependencyStore.Gui
     [STAThread]
     public static void Main()
     {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new MainForm());
+      using (DependencyStoreContainer container = new DependencyStoreContainer())
+      {
+        container.Initialize();
+        container.PrepareForServices();
+        container.Start();
+        container.Add<ConfigurationPaths>();
+        container.Add<MainForm>();
+
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(container.Resolve.Object<MainForm>());
+      }
     }
   }
 }
