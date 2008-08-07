82f9c38efe9a925631d1aab6eb7dbbc115f77f9e
Jacob Lewallen <jlewallen@gmail.com>
2008-08-06 17:14:29 -0700

    Adding DependencyState class that handles all the fetching gathering.
    It's used by the StatusController, and may be promoted elsewhere. It
    basically gathers are useful program state when doing dependency
    update procedures.

--- DependencyStore.Gui/StatusController.cs
+++ DependencyStore.Gui/StatusController.cs
@@ -8,10 +8,76 @@ using Machine.Core.Services;
 
 namespace DependencyStore.Gui
 {
+  public class DependencyState
+  {
+    private readonly ILocationRepository _locationRepository;
+    private readonly IConfigurationRepository _configurationRepository;
+    private readonly IFileAndDirectoryRulesRepository _fileAndDirectoryRulesRepository;
+    private readonly ConfigurationPaths _configurationPaths;
+    private DependencyStoreConfiguration _configuration;
+    private FileAndDirectoryRules _rules;
+    private IList<SourceLocation> _sources;
+    private IList<SinkLocation> _sinks;
+    private LatestFileSet _latestFiles;
+
+    public LatestFileSet LatestFiles
+    {
+      get { return _latestFiles; }
+    }
+
+    public IList<SinkLocation> Sinks
+    {
+      get { return _sinks; }
+    }
+
+    public IList<SourceLocation> Sources
+    {
+      get { return _sources; }
+    }
+
+    public FileSetGroupedByLocation LatestFilesGroupByLocation
+    {
+      get { return FileSetGroupedByLocation.GroupFileSetIntoLocations(_sources, _latestFiles); }
+    }
+
+    public SynchronizationPlan SynchronizationPlan
+    {
+      get
+      {
+        SynchronizationPlan plan = new SynchronizationPlan();
+        foreach (SinkLocation location in _sinks)
+        {
+          plan.Merge(location.CreateSynchronizationPlan(this.LatestFiles));
+        }
+        return plan;
+      }
+    }
+
+    public DependencyState(ILocationRepository locationRepository, IConfigurationRepository configurationRepository, IFileAndDirectoryRulesRepository fileAndDirectoryRulesRepository, ConfigurationPaths configurationPaths)
+    {
+      _locationRepository = locationRepository;
+      _configurationPaths = configurationPaths;
+      _fileAndDirectoryRulesRepository = fileAndDirectoryRulesRepository;
+      _configurationRepository = configurationRepository;
+    }
+
+    public void Refresh()
+    {
+      string path = _configurationPaths.FindConfigurationPath();
+      _configuration = _configurationRepository.FindConfiguration(path);
+      _rules = _fileAndDirectoryRulesRepository.FindDefault();
+      _sinks = _locationRepository.FindAllSinks(_configuration, _rules);
+      _sources = _locationRepository.FindAllSources(_configuration, _rules);
+      _latestFiles = new LatestFileSet();
+      _latestFiles.AddAll(_sources);
+      _latestFiles.SortByModifiedAt();
+    }
+  }
   public class StatusController
   {
     private readonly IStatusView _view;
     private readonly ConfigurationPaths _configurationPaths;
+    private readonly DependencyState _state;
     private readonly IConfigurationRepository _configurationRepository;
     private readonly IFileAndDirectoryRulesRepository _fileAndDirectoryRulesRepository;
     private readonly ILocationRepository _locationRepository;
@@ -25,6 +91,7 @@ namespace DependencyStore.Gui
       _fileAndDirectoryRulesRepository = fileAndDirectoryRulesRepository;
       _configurationRepository = configurationRepository;
       _configurationPaths = configurationPaths;
+      _state = new DependencyState(_locationRepository, _configurationRepository, _fileAndDirectoryRulesRepository, _configurationPaths);
     }
 
     public void Start()
@@ -34,50 +101,18 @@ namespace DependencyStore.Gui
 
     public void UpdateView()
     {
-      LatestFileSet latestFiles = GetLatestFiles();
-      FileAndDirectoryRules rules = _fileAndDirectoryRulesRepository.FindDefault();
-      IList<SourceLocation> sources = _locationRepository.FindAllSources(GetConfiguration(), rules);
-      FileSetGroupedByLocation groupedByLocation = FileSetGroupedByLocation.GroupFileSetIntoLocations(sources, latestFiles);
-      _view.LatestFiles = groupedByLocation;
-      _view.SynchronizationPlan = GetSynchronizationPlan(latestFiles);
+      _state.Refresh();
+      _view.LatestFiles = _state.LatestFilesGroupByLocation;
+      _view.SynchronizationPlan = _state.SynchronizationPlan;
     }
 
     private void OnSynchronize(object sender, EventArgs e)
     {
-      LatestFileSet latestFiles = GetLatestFiles();
-      SynchronizationPlan plan = GetSynchronizationPlan(latestFiles);
-      foreach (UpdateOutOfDateFile update in plan)
+      _state.Refresh();
+      foreach (UpdateOutOfDateFile update in _state.SynchronizationPlan)
       {
         _fileSystem.CopyFile(update.SourceFile.Purl.AsString, update.SinkFile.Purl.AsString, true);
       }
-    }
-
-    private LatestFileSet GetLatestFiles()
-    {
-      FileAndDirectoryRules rules = _fileAndDirectoryRulesRepository.FindDefault();
-      IList<SourceLocation> sources = _locationRepository.FindAllSources(GetConfiguration(), rules);
-      LatestFileSet latestFiles = new LatestFileSet();
-      latestFiles.AddAll(sources);
-      latestFiles.SortByModifiedAt();
-      return latestFiles;
-    }
-
-    private SynchronizationPlan GetSynchronizationPlan(LatestFileSet latestFiles)
-    {
-      FileAndDirectoryRules rules = _fileAndDirectoryRulesRepository.FindDefault();
-      SynchronizationPlan plan = new SynchronizationPlan();
-      IList<SinkLocation> sinks = _locationRepository.FindAllSinks(GetConfiguration(), rules);
-      foreach (SinkLocation location in sinks)
-      {
-        plan.Merge(location.CreateSynchronizationPlan(latestFiles));
-      }
-      return plan;
-    }
-
-    private DependencyStoreConfiguration GetConfiguration()
-    {
-      string path = _configurationPaths.FindConfigurationPath();
-      return _configurationRepository.FindConfiguration(path);
     }
   }
 }
