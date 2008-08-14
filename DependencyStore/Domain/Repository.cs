b538df0f17eed88b94c150e9fe0a953c66df1532
Jacob Lewallen <jlewallen@gmail.com>
2008-08-13 17:31:12 -0700

    Fleshing out details on the ArchivedProject's and their versions.
    Pretty basic relationships between everything. Not a whole lot of
    logic in here yet. My big complaint here is that because I plan to use
    XML Serialization to speed things along I have to mess up how I
    have my classes written (setters, etc...)

--- DependencyStore/Domain/Repository.cs
+++ DependencyStore/Domain/Repository.cs
@@ -5,5 +5,105 @@ namespace DependencyStore.Domain
 {
   public class Repository
   {
+    private readonly List<ArchivedProject> _projects = new List<ArchivedProject>();
+
+    public List<ArchivedProject> Projects
+    {
+      get { return _projects; }
+    }
+
+    public ArchivedProject FindProject(string name)
+    {
+      foreach (ArchivedProject existing in _projects)
+      {
+        if (existing.Name.Equals(name))
+        {
+          return existing;
+        }
+      }
+      return null;
+    }
+
+    public void AddProject(ArchivedProject project)
+    {
+      if (FindProject(project.Name) != null)
+      {
+        throw new InvalidOperationException();
+      }
+      _projects.Add(project);
+    }
+  }
+  public class ArchivedProject
+  {
+    private readonly List<ArchivedProjectVersion> _versions = new List<ArchivedProjectVersion>();
+    private string _name;
+
+    public string Name
+    {
+      get { return _name; }
+      set { _name = value; }
+    }
+
+    public List<ArchivedProjectVersion> Versions
+    {
+      get { return _versions; }
+    }
+
+    public ArchivedProject()
+    {
+    }
+
+    public ArchivedProject(string name)
+    {
+      _name = name;
+    }
+
+    public ArchivedProjectVersion FindVersion(string version)
+    {
+      foreach (ArchivedProjectVersion existing in _versions)
+      {
+        if (existing.Version.Equals(version))
+        {
+          return existing;
+        }
+      }
+      return null;
+    }
+
+    public void AddVersion(ArchivedProjectVersion version)
+    {
+      if (FindVersion(version.Version) != null)
+      {
+        throw new InvalidOperationException();
+      }
+      _versions.Add(version);
+    }
+  }
+  public class ArchivedProjectVersion
+  {
+    private string _version;
+    private DateTime _createdAt;
+
+    public string Version
+    {
+      get { return _version; }
+      set { _version = value; }
+    }
+
+    public DateTime CreatedAt
+    {
+      get { return _createdAt; }
+      set { _createdAt = value; }
+    }
+
+    public ArchivedProjectVersion()
+    {
+    }
+
+    public ArchivedProjectVersion(DateTime createdAt, string version)
+    {
+      _createdAt = createdAt;
+      _version = version;
+    }
   }
 }
