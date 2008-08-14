96be1845a56d7241adaa04426075500ac9c17ca0
Jacob Lewallen <jlewallen@gmail.com>
2008-08-13 17:49:18 -0700

    Implementing the repository repository a little more.
    It's just wrappers around XmlSerializationHelper for now. Returns an empty
    repository if the file can't be found.

--- DependencyStore/Services/DataAccess/IRepositoryRepository.cs
+++ DependencyStore/Services/DataAccess/IRepositoryRepository.cs
@@ -9,5 +9,6 @@ namespace DependencyStore.Services.DataAccess
   public interface IRepositoryRepository
   {
     Repository FindDefaultRepository(DependencyStoreConfiguration configuration);
+    void SaveRepository(Repository repository, DependencyStoreConfiguration configuration);
   }
 }
--- DependencyStore/Services/DataAccess/Impl/RepositoryRepository.cs
+++ DependencyStore/Services/DataAccess/Impl/RepositoryRepository.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 using DependencyStore.Domain;
 using DependencyStore.Domain.Configuration;
+
+using Machine.Core.Services;
+using Machine.Core.Utility;
 
 namespace DependencyStore.Services.DataAccess.Impl
 {
   public class RepositoryRepository : IRepositoryRepository
   {
+    private readonly IFileSystem _fileSystem;
+
+    public RepositoryRepository(IFileSystem fileSystem)
+    {
+      _fileSystem = fileSystem;
+    }
+
     #region IRepositoryRepository Members
     public Repository FindDefaultRepository(DependencyStoreConfiguration configuration)
     {
-      Repository repository = new Repository();
-      return repository;
+      Purl path = configuration.RepositoryDirectory.Join("Manifest.xml");
+      if (!_fileSystem.IsFile(path.AsString))
+      {
+        return new Repository();
+      }
+      using (StreamReader stream = new StreamReader(_fileSystem.OpenFile(path.AsString)))
+      {
+        return XmlSerializationHelper.DeserializeString<Repository>(stream.ReadToEnd());
+      }
+    }
+
+    public void SaveRepository(Repository repository, DependencyStoreConfiguration configuration)
+    {
+      Purl path = configuration.RepositoryDirectory.Join("Manifest.xml");
+      using (StreamWriter stream = new StreamWriter(_fileSystem.CreateFile(path.AsString)))
+      {
+        stream.Write(XmlSerializationHelper.Serialize(repository));
+      }
     }
     #endregion
   }
