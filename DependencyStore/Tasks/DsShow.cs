d91f2d6dfff74ea5ab49584a4231203b3bac816f
Jacob Lewallen <jlewallen@gmail.com>
2008-08-15 08:12:45 -0700

    Nuked old MsBuild task stuff.

---
+++ DependencyStore/Tasks/DsShow.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace DependencyStore.Tasks
+{
+  public class DsShow : DsTask
+  {
+    public override bool Execute()
+    {
+      return true;
+    }
+  }
+}
--- DependencyStore/Tasks/DsTask.cs
+++ DependencyStore/Tasks/DsTask.cs
@@ -9,6 +9,7 @@ namespace DependencyStore.Tasks
   public abstract class DsTask : Task
   {
     private string _projectName;
+    private bool _dryRun;
 
     [Required]
     public string ProjectName
@@ -16,5 +17,13 @@ namespace DependencyStore.Tasks
       get { return _projectName; }
       set { _projectName = value; }
     }
+
+    [Required]
+    public bool DryRun
+    {
+      get { return _dryRun; }
+      set { _dryRun = value; }
+    }
+
   }
 }
