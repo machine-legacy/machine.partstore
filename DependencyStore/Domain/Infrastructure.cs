d5a5f87f8ad02b732edffec0124c5af5874876bf
Jacob Lewallen <jlewallen@gmail.com>
2008-08-14 19:11:43 -0700

    Adding Infrastructure static accessor for FileSystem and such.

---
+++ DependencyStore/Domain/Infrastructure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Machine.Container;
+using Machine.Core.Services;
+
+namespace DependencyStore.Domain
+{
+  public static class Infrastructure
+  {
+    public static IFileSystem FileSystem
+    {
+      get { return IoC.Container.Resolve.Object<IFileSystem>(); }
+    }
+  }
+}
