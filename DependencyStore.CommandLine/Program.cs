fe55e1b7fa35840c1e282a38f3497eda71383f23
Jacob Lewallen <jlewallen@gmail.com>
2008-08-16 16:12:13 -0700

    Deleting a bunch of stuff that's commented and will go away.

--- DependencyStore.CommandLine/Program.cs
+++ DependencyStore.CommandLine/Program.cs
@@ -2,11 +2,7 @@ using System;
 using System.Collections.Generic;
 
 using Machine.Container;
-using Machine.Container.Services;
 
-using DependencyStore.Services;
-using DependencyStore.Domain.Repositories;
-using DependencyStore.Domain.Configuration;
 using DependencyStore.Services.DataAccess;
 using DependencyStore.Commands;
 
@@ -43,63 +39,6 @@ namespace DependencyStore.CommandLine
         ICommand command = commandFactory.CreateCommand(commandName);
         IConfigurationRepository configurationRepository = container.Resolve.Object<IConfigurationRepository>();
         command.Run(configurationRepository.FindDefaultConfiguration());
-        /*
-        IConfigurationRepository configurationRepository = container.Resolve.Object<IConfigurationRepository>();
-        bool dryRun = false;
-        bool archiving = false;
-        bool unpack = false;
-        bool upgrade = false;
-        foreach (string arg in args)
-        {
-          if (arg == "--archive")
-          {
-            archiving = true;
-          }
-          if (arg == "--unpack")
-          {
-            unpack = true;
-          }
-          if (arg == "--upgrade")
-          {
-            upgrade = true;
-          }
-          if (arg == "--dry")
-          {
-            dryRun = true;
-          }
-        }
-        IController controller = container.Resolve.Object<IController>();
-        DependencyStoreConfiguration configuration = configurationRepository.FindDefaultConfiguration();
-        if (archiving)
-        {
-          IRepositoryRepository repositoryRepository = container.Resolve.Object<IRepositoryRepository>();
-          Repository repository = repositoryRepository.FindDefaultRepository(configuration);
-          controller.AddLatestToRepository(configuration, repository);
-          repositoryRepository.SaveRepository(repository, configuration);
-        }
-        else if (unpack)
-        {
-          IRepositoryRepository repositoryRepository = container.Resolve.Object<IRepositoryRepository>();
-          Repository repository = repositoryRepository.FindDefaultRepository(configuration);
-          controller.Unpack(configuration, repository);
-        }
-        else if (upgrade)
-        {
-          IRepositoryRepository repositoryRepository = container.Resolve.Object<IRepositoryRepository>();
-          Repository repository = repositoryRepository.FindDefaultRepository(configuration);
-          controller.Upgrade(configuration, repository);
-        }
-        else
-        {
-          if (dryRun)
-          {
-            controller.Show(configuration);
-          }
-          else
-          {
-            controller.Update(configuration);
-          }
-        }*/
       }
     }
   }
