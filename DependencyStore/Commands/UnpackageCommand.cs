717a7fe803552442dca7b03d913689170611013a
Jacob Lewallen <jlewallen@gmail.com>
2008-09-18 00:08:48 -0700

    Notify of the unpackage progress.

--- DependencyStore/Commands/UnpackageCommand.cs
+++ DependencyStore/Commands/UnpackageCommand.cs
@@ -1,5 +1,5 @@
 using System;
-
+using DependencyStore.Domain.Archiving;
 using DependencyStore.Domain.Distribution;
 using DependencyStore.Domain.Distribution.Repositories;
 
@@ -16,9 +16,20 @@ namespace DependencyStore.Commands
 
     public override CommandStatus Run()
     {
+      ArchivingDomainEvents.Progress += OnProgress;
       CurrentProject project = _currentProjectRepository.FindCurrentProject();
       project.UnpackageIfNecessary();
       return CommandStatus.Success;
+    }
+
+    private static void OnProgress(object sender, ProgressEventArgs e)
+    {
+      ArchiveFileProgressEventArgs archiveArgs = (ArchiveFileProgressEventArgs)e;
+      Console.Write("Unzipping {0:##.##}%\r", archiveArgs.PercentComplete * 100.0);
+      if (e.PercentComplete == 1.0)
+      {
+        Console.WriteLine("Archive unpackaged: {0}", archiveArgs.Archive.Name);
+      }
     }
   }
 }
