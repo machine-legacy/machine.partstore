cbf30ec46fa383b597687058c8315ad35506101e
Jacob Lewallen <jlewallen@gmail.com>
2008-10-01 23:03:56 -0700

    No need to use this namespace.

--- DependencyStore/Domain/Core/CurrentProject.cs
+++ DependencyStore/Domain/Core/CurrentProject.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using DependencyStore.Domain.Core.Repositories.Impl;
+
 using DependencyStore.Domain.FileSystem;
 
 namespace DependencyStore.Domain.Core
