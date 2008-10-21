da180914db9043bbb7d4cb40d54b6b4f6e4a0898
Jacob Lewallen <jlewallen@gmail.com>
2008-10-20 19:06:04 -0700

    Improving the specification's handling of directories and such.
    Also implemented the some basic Configuring specifications.

---
+++ DependencyStore.Specs/Configuring.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+
+using DependencyStore.Commands;
+
+using Machine.Specifications;
+
+namespace DependencyStore
+{
+  public class with_configure_command : with_testing_repository_and_blank_project
+  {
+    protected static ConfigureCommand command;
+
+    Establish context = () =>
+      command = container.Resolve.Object<ConfigureCommand>();
+  }
+
+  [Subject("Configuring")]
+  public class when_configuring_a_new_project_with_no_root_directory_clue : with_configure_command
+  {
+    static CommandStatus status;
+    static Exception error;
+
+    Establish context = () =>
+      project.RemoveRootClue();
+
+    Because of = () =>
+      error = Catch.Exception(() => status = command.Run());
+
+    It should_fail = () =>
+      error.ShouldBeOfType<InvalidOperationException>();
+  }
+
+  [Subject("Configuring")]
+  public class when_configuring_a_new_project : with_configure_command
+  {
+    static CommandStatus status;
+
+    Because of = () =>
+      status = command.Run();
+
+    It should_succeed = () =>
+      status.ShouldEqual(CommandStatus.Success);
+
+    It should_write_configuration_file = () =>
+      project.HasConfiguration.ShouldBeTrue();
+  }
+
+  [Subject("Configuring")]
+  public class when_configuring_an_existing_project : with_configure_command
+  {
+    static CommandStatus status;
+
+    Because of = () =>
+      status = command.Run();
+
+    It should_succeed = () =>
+      status.ShouldEqual(CommandStatus.Success);
+
+    It should_write_configuration_file = () =>
+      project.HasConfiguration.ShouldBeTrue();
+  }
+}
--- DependencyStore.Specs/With_testing_repository.cs
+++ DependencyStore.Specs/With_testing_repository.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-
+using System.IO;
 using Machine.Container;
 using Machine.Specifications;
 
@@ -24,21 +24,32 @@ namespace DependencyStore
 
   public class with_testing_repository : with_container
   {
-    static TestingRepository repository;
+    protected static TestingRepository repository;
 
     Establish context = () =>
     {
       repository = new TestingRepository();
     };
+
+    Cleanup after = () =>
+    {
+      repository.Cleanup();
+    };
   }
 
   public class with_testing_repository_and_blank_project : with_testing_repository
   {
-    static TestingProject project;
+    protected static TestingProject project;
 
     Establish context = () =>
     {
       project = new TestingProject();
+      project.Create();
+    };
+
+    Cleanup after = () =>
+    {
+      project.Cleanup();
     };
   }
 
@@ -46,13 +57,20 @@ namespace DependencyStore
   {
     private readonly string _directory;
 
-    public string Directory
+    public string RootDirectory
     {
       get { return _directory; }
     }
 
     public TestingRepository()
     {
+      _directory = Path.Combine(Path.GetTempPath(), "TestRepository");
+      Directory.CreateDirectory(_directory);
+    }
+
+    public void Cleanup()
+    {
+      Directory.Delete(_directory);
     }
   }
 
@@ -60,13 +78,51 @@ namespace DependencyStore
   {
     private readonly string _directory;
 
-    public string Directory
+    public string RootDirectory
     {
       get { return _directory; }
     }
 
+    public bool HasConfiguration
+    {
+      get { return File.Exists(PathTo("DependencyStore.config")); }
+    }
+
     public TestingProject()
     {
+      _directory = Path.Combine(Path.GetTempPath(), "TestProject");
+    }
+
+    private string PathTo(string path)
+    {
+      return Path.Combine(_directory, path);
+    }
+
+    public void Create()
+    {
+      Environment.CurrentDirectory = Environment.GetFolderPath(Environment.SpecialFolder.System);
+      if (Directory.Exists(_directory))
+      {
+        Directory.Delete(_directory, true);
+      }
+      Directory.CreateDirectory(_directory);
+      Environment.CurrentDirectory = _directory;
+      AddRootClue();
+    }
+
+    public void RemoveRootClue()
+    {
+      File.Delete(PathTo(".gitignore"));
+    }
+
+    public void AddRootClue()
+    {
+      File.WriteAllText(PathTo(".gitignore"), String.Empty);
+    }
+
+    public void Cleanup()
+    {
+      Directory.Delete(_directory);
     }
   }
 }
