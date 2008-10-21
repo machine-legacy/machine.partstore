111e929dd2a75c7d3f23372a7670953c593b0124
Jacob Lewallen <jlewallen@gmail.com>
2008-10-20 18:31:28 -0700

    Adding Machine.Specifications.

---
+++ DependencyStore.Specs/With_testing_repository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using Machine.Specifications;
+
+namespace DependencyStore
+{
+  public class with_testing_repository
+  {
+    Establish context = () =>
+    {
+    };
+  }
+}
