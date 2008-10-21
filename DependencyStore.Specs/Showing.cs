5c62d17d09c07c7b3423b204746e00213e84319d
Jacob Lewallen <jlewallen@gmail.com>
2008-10-20 19:08:32 -0700

    Adding ShowCommand specifications, we'll beef this up as we do more.
    Since we'll need to be able to run Show under varrying situations and
    verify we're seeing the things we expect we'll have to revisit this.

---
+++ DependencyStore.Specs/Showing.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using DependencyStore.Commands;
+using Machine.Specifications;
+
+namespace DependencyStore
+{
+  public class with_show_command : with_testing_repository_and_blank_project
+  {
+    protected static ShowCommand command;
+
+    Establish context = () =>
+      command = container.Resolve.Object<ShowCommand>();
+  }
+
+  [Subject("Configuring")]
+  public class when_showing_a_blank_directory : with_show_command
+  {
+    static CommandStatus status;
+    static Exception error;
+
+    Because of = () =>
+      status = command.Run();
+
+    It should_fail = () =>
+      status.ShouldEqual(CommandStatus.Failure);
+  }
+
+  [Subject("Configuring")]
+  public class when_showing_blank_project : with_configure_command
+  {
+    static CommandStatus status;
+
+    Because of = () =>
+      status = command.Run();
+
+    It should_succeed = () =>
+      status.ShouldEqual(CommandStatus.Success);
+  }
+}
